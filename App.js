5587dbc486b8e848806424efa5cceac60f75450d
Dylan Mason <dylanmason@pop-os.localdomain>
2022-09-23 10:03:06 -0400

    Changed to imperial units

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default function App() {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
     const [weather, setWeather] = useState(null);
+    const [temperature, setTemperature] = useState(null);
 
     useEffect(() => {
         (async () => {
@@ -21,7 +22,7 @@ export default function App() {
             setLocation(location);
             const latitude = location.coords.latitude;
             const longitude = location.coords.longitude;
-            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1c384d0483290f4ddf94960bfee68f6b`);
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1c384d0483290f4ddf94960bfee68f6bi&units=imperial`);
             const jsonInfo = await data.json();
             console.log(jsonInfo);
             setWeather(jsonInfo);
