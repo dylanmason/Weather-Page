c6a9293f0248dbde0a989f09229ed1f575fa8b25
Dylan Mason <dylanmason@pop-os.localdomain>
2022-09-22 20:24:55 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import React, {useEffect, useState} from 'react';
+
+export default function App() {
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            const latitude = location.coords.latitude;
+            const longitude = location.coords.longitude;
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1c384d0483290f4ddf94960bfee68f6b`);
+            const jsonInfo = await data.json();
+            console.log(jsonInfo);
+            setWeather(jsonInfo);
+
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (weather) {
+        text = JSON.stringify(weather);
+    }
+
+
+    return (
+        <View style={styles.container}>
+        <Text>{text}</Text>
+        <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
