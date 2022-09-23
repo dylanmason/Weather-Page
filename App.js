df2ea8157ebeaec7a17b774abeca880255566dc7
Dylan Mason <dylanmason@pop-os.localdomain>
2022-09-23 12:09:15 -0400

    Formatted the json data I currently want and added animations to fade the data in

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Animated } from 'react-native';
 import * as Location from 'expo-location';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import { Ionicons } from '@expo/vector-icons'; 
 
 export default function App() {
+
+    const units = "imperial";
+    const key = "1c384d0483290f4ddf94960bfee68f6b";
 
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
     const [weather, setWeather] = useState(null);
-    const [temperature, setTemperature] = useState(null);
+    const [town, setTown] = useState(null);
+    const [currentTemperature, setCurrentTemperature] = useState(null);
+    const [feelsLike, setFeelsLike] = useState(null);
+    const fadeAnimation = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () => {
+        Animated.timing(fadeAnimation, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true
+        }).start();
+    };
+
+    const Icon = () => {
+        if (weather === "clear sky") {
+            return (
+                <Ionicons name="ios-sunny-outline" size={48} color='hsl(204, 100%, 90%)' />
+            );
+        }
+    };
 
     useEffect(() => {
         (async () => {
@@ -22,11 +45,15 @@ export default function App() {
             setLocation(location);
             const latitude = location.coords.latitude;
             const longitude = location.coords.longitude;
-            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1c384d0483290f4ddf94960bfee68f6bi&units=imperial`);
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${key}`);
             const jsonInfo = await data.json();
             console.log(jsonInfo);
-            setWeather(jsonInfo);
+            setWeather(jsonInfo.weather[0].description);
+            setTown(jsonInfo.name);
+            setCurrentTemperature(jsonInfo.main.temp);
+            setFeelsLike(jsonInfo.main.feels_like);
 
+            fadeIn();
         })();
     }, []);
 
@@ -40,17 +67,38 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-        <Text>{text}</Text>
-        <StatusBar style="auto" />
+        <Animated.View style={[
+            styles.container,
+            {
+                opacity: fadeAnimation,
+            },
+        ]}>
+        <Icon />
+        <Text style={styles.townText}>{town}</Text>
+        <Text style={styles.currentTemperatureText}>{currentTemperature}&deg;F</Text>
+        <Text style={styles.feelsLikeText}>{feelsLike}&deg;F</Text>
+        </Animated.View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+        flex: 1,
+        backgroundColor: 'hsl(204, 100%, 50%)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    currentTemperatureText: {
+        color: 'hsl(204, 100%, 90%)',
+        fontSize: 48,
+    },
+    feelsLikeText: {
+        color: 'hsl(204, 100%, 90%)',
+        fontSize: 14,
+    },
+    townText: {
+        color: 'hsl(204, 100%, 90%)',
+        fontSize: 32,
+    },
 });
